e665b157e1e8253452eb07312f28e52edbfe8325
mateonunez <mateonunez95@gmail.com>
2022-03-27 16:37:37 +0200

    feat(Icons): add main icons

---
+++ components/icons/index.js
@@ -0,0 +1,5 @@
+export { default as MN } from './MN';
+export { default as Github } from './Github';
+export { default as LinkedIn } from './LinkedIn';
+export { default as Instagram } from './Instagram';
+export { default as Spotify } from './Spotify';
