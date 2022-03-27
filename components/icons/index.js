6323d162501d49fedb3d274fffb32a67cf21767d
mateonunez <mateonunez95@gmail.com>
2022-03-27 17:04:58 +0200

    feat(Header): create basic component

--- components/icons/index.js
+++ components/icons/index.js
@@ -3,3 +3,5 @@ export { default as Github } from './Github';
 export { default as LinkedIn } from './LinkedIn';
 export { default as Instagram } from './Instagram';
 export { default as Spotify } from './Spotify';
+export { default as ChevronDown } from './ChevronDown';
+export { default as ChevronUp } from './ChevronUp';
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import heroStyles from 'styles/pages/home/hero.module.css';
 
 import Terminal from 'components/terminal';
+import Header from 'components/header';
 
 export default function Home() {
   return (
     <>
+      <Header />
+
       <div className="container">
         {/* Hero  */}
         <div className={heroStyles.root}>
