d8821b86dbb34a9352c7a4b9c07ee497724cb768
mateonunez <mateonunez95@gmail.com>
2022-03-27 15:35:01 +0200

    feat(Homepage): add hero and terminal

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import heroStyles from 'styles/pages/home/hero.module.css';
+
+import Terminal from 'components/terminal';
+
 export default function Home() {
   return (
     <>
-      <div>Mateo Nunez</div>
+      <div className="container">
+        {/* Hero  */}
+        <div className={heroStyles.root}>
+          {/* Title  */}
+          <h1 className={heroStyles.title}>Mateo Nunez</h1>
+
+          {/* Terminal  */}
+          <div className={heroStyles.terminal}>
+            <Terminal />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,45 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      animation: {
+        'gradient-x': 'gradient-x 15s ease infinite',
+        'gradient-y': 'gradient-y 15s ease infinite',
+        'gradient-xy': 'gradient-xy 15s ease infinite'
+      },
+      keyframes: {
+        'gradient-y': {
+          '0%, 100%': {
+            'background-size': '400% 400%',
+            'background-position': 'center top'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'center center'
+          }
+        },
+        'gradient-x': {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          }
+        },
+        'gradient-xy': {
+          '0%, 100%': {
+            'background-size': '400% 400%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          }
+        }
+      }
+    }
   },
   plugins: []
 };
