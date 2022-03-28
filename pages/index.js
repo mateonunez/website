5cccc245218989e4bd9fdef5597b9cf5fdcc016d
mateonunez <mateonunez95@gmail.com>
2022-03-28 11:58:35 +0200

    feat(Homepage): add hero transitions and height

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,32 @@
 import s from 'styles/pages/home.module.css';
 
+import { useUI } from 'components/ui/UIContext';
+import cn from 'classnames';
+
 import Header from 'components/header';
 import Hero from 'components/hero';
-import { useUI } from 'components/ui/UIContext';
+import { useEffect } from 'react';
 
 export default function Home() {
   const { bigBang } = useUI();
+
+  useEffect(() => {
+    console.log(bigBang);
+  }, [bigBang]);
 
   return (
     <>
       <Header />
 
       <div className={s.root}>
-        <Hero />
+        <Hero
+          className={cn(
+            'flex flex-col transition ease-in-out duration-300',
+            bigBang ? 'opacity-0 h-0' : 'opacity-100 h-full'
+          )}
+        />
 
-        {bigBang && <div className={s.bigBang}>Big Bang</div>}
+        {bigBang && <div className={s.universe}>BigBang</div>}
       </div>
     </>
   );
