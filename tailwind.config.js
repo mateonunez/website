e6805693c708aedcaeb96ed033798a698e77fe65
mateonunez <mateonunez95@gmail.com>
2022-03-29 12:55:35 +0200

    chore(Hero): improve gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        'gradient-x': 'gradient-x 15s ease infinite',
-        'gradient-y': 'gradient-y 15s ease infinite',
-        'gradient-xy': 'gradient-xy 15s ease infinite'
+        'gradient-x': 'gradient-x 10s ease infinite',
+        'gradient-y': 'gradient-y 10s ease infinite',
+        'gradient-xy': 'gradient-xy 10s ease infinite'
       },
       keyframes: {
         'gradient-y': {
