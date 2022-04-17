b6383fed7b5fe225bcc14444ecb1df8764362078
mateonunez <mateonunez95@gmail.com>
2022-04-17 12:01:32 +0200

    feat(MDX): add heading components

---
+++ components/articles/mdx/heading/index.js
@@ -0,0 +1,39 @@
+export const H1 = ({ children, ...rest }) => (
+  <>
+    <h1 title={children} aria-label={children} {...rest}>
+      {children}
+    </h1>
+  </>
+);
+
+export const H2 = ({ children, ...rest }) => (
+  <>
+    <h2 title={children} aria-label={children} {...rest}>
+      {children}
+    </h2>
+  </>
+);
+
+export const H3 = ({ children, ...rest }) => (
+  <>
+    <h3 title={children} aria-label={children} {...rest}>
+      {children}
+    </h3>
+  </>
+);
+
+export const H4 = ({ children, ...rest }) => (
+  <>
+    <h4 title={children} aria-label={children} {...rest}>
+      {children}
+    </h4>
+  </>
+);
+
+export const H5 = ({ children, ...rest }) => (
+  <>
+    <h5 title={children} aria-label={children} {...rest}>
+      {children}
+    </h5>
+  </>
+);
--- components/articles/mdx/index.js
+++ components/articles/mdx/index.js
@@ -1,1 +1,2 @@
-export { default as Image } from './MDXImage';
+export { default as Image } from './image';
+export { H1 as h1, H2 as h2, H3 as h3, H4 as h4, H5 as h5 } from './heading';
