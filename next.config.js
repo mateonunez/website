ae4bbcde1d5a466e30a9b97fad4f474d41fdb4d1
mateonunez <mateonunez95@gmail.com>
2022-05-15 09:30:06 +0200

    feat(*): enable hot reload

--- next.config.js
+++ next.config.js
@@ -3,5 +3,12 @@ module.exports = {
   images: {
     formats: ['image/avif', 'image/webp'],
     domains: ['i.scdn.co']
-  }
+  },
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
