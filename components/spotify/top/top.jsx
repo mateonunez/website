f34fea28cc63f8df9573259985eb411ce00127a0
mateonunez <mateonunez95@gmail.com>
2022-06-05 11:55:19 +0200

    feat(Top): create spotify component

---
+++ components/spotify/top/top.jsx
@@ -0,0 +1,83 @@
+import s from './top.module.css';
+
+import { ArtistCard, TrackCard, Container, Fade, Title } from 'components';
+import config from 'lib/config';
+import { useRef } from 'react';
+
+export default function Top({ artists, tracks }) {
+  const artistTitleRef = useRef(null);
+  const artistsRef = useRef(null);
+
+  const tracksTitleRef = useRef(null);
+  const tracksRef = useRef(null);
+
+  return (
+    <>
+      <Container className="min-h-screen" clean>
+        <div className="py-8">
+          <Fade delay={config.munber / 10}>
+            <Title>Top of this month</Title>
+          </Fade>
+        </div>
+
+        <div className={s.root}>
+          {/* Artists  */}
+          {artists?.length > 0 && (
+            <>
+              <div ref={artistTitleRef}>
+                <Fade direction="left" trigger={artistTitleRef}>
+                  <Title className="text-2xl" variant="naked" element="h4">
+                    Artists
+                  </Title>
+                </Fade>
+              </div>
+
+              {/* Artists Container  */}
+              <div className={s['artists-container']} ref={artistsRef}>
+                {artists.map((artist, key) => (
+                  <Fade
+                    key={`${artist.id}-${new Date().getTime()}`}
+                    className={s.artist}
+                    delay={key - config.munber / 100}
+                    trigger={artistsRef}
+                    clean>
+                    <ArtistCard item={artist} delay={key + config.munber / 100} />
+                  </Fade>
+                ))}
+              </div>
+            </>
+          )}
+
+          {/* Tracks  */}
+          {tracks?.length > 0 && (
+            <>
+              <div ref={tracksTitleRef}>
+                <Fade delay={config.munber / 100} direction="right" trigger={tracksTitleRef}>
+                  <Title className="text-2xl" variant="naked" element="h4">
+                    Tracks
+                  </Title>
+                </Fade>
+              </div>
+
+              <div className={s['tracks-container']} ref={tracksRef}>
+                {/* Tracks Container  */}
+                {tracks.map((track, key) => (
+                  <>
+                    <Fade
+                      key={`${track.id}-${new Date().getTime()}`}
+                      className={s.track}
+                      delay={key - config.munber / 100}
+                      clean
+                      trigger={tracksRef}>
+                      <TrackCard item={track} delay={key + config.munber / 100} variant="top" />
+                    </Fade>
+                  </>
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+      </Container>
+    </>
+  );
+}
