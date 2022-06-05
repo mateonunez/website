53b43fa0e6b4eb54a71704e03133659c85ad9f6b
mateonunez <mateonunez95@gmail.com>
2022-06-05 10:48:26 +0200

    feat(Spotify): create Top API

---
+++ pages/api/spotify/top.jsx
@@ -0,0 +1,38 @@
+import config from 'lib/config';
+import { getTopArtists, getTopTracks } from 'lib/spotify';
+import { normalizeArtists, normalizeTracks } from 'lib/utils/normalizers';
+
+export default async function handler(req, res) {
+  const artistsResponse = await getTopArtists();
+  const tracksResponse = await getTopTracks();
+
+  if (!artistsResponse || !tracksResponse) {
+    return res.status(500).json({ error: 'Spotify not available' });
+  }
+
+  if (
+    artistsResponse.status === 204 ||
+    artistsResponse.status > 400 ||
+    tracksResponse.status === 204 ||
+    tracksResponse.status > 400
+  ) {
+    // TODO handle better this
+    return res.status(200).json({ recently_played: false });
+  }
+
+  const { items: artists = [] } = await artistsResponse.json();
+  const { items: tracks = [] } = await tracksResponse.json();
+
+  return res.status(200).json({
+    artists: artists.map(normalizeArtists),
+    tracks: tracks.map(normalizeTracks)
+  });
+}
+
+export async function topFetcher() {
+  const topResponse = await fetch(`${config.baseUrl}/api/spotify/top`);
+
+  const top = await topResponse.json();
+
+  return top;
+}
