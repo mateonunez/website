2d8d9cc5854cf682a4777b578e34cd84f8bfff67
mateonunez <mateonunez95@gmail.com>
2022-08-07 12:40:32 +0200

    feat(open-source): add github components

--- pages/open-source/index.jsx
+++ pages/open-source/index.jsx
@@ -1,13 +1,7 @@
-import * as components from 'components/articles/mdx';
-
-import { Container, Footer, Header, Title } from 'components';
-import { Profile as GitHubProfile } from 'components/github/cards/profile';
-import { MDXRemote } from 'next-mdx-remote';
+import { Container, GitHubProfile, Footer, Header, RepositoryPreview, Title } from 'components';
 import { NextSeo } from 'next-seo';
 
-import { readmeFetcher } from 'pages/api/open-source/readme';
 import { profileFetcher } from 'pages/api/open-source/profile';
-import { compileSource, minifySource, parseArticleData } from 'lib/articles/parser';
 
 export async function getServerSideProps({ res }) {
   res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59');
@@ -17,16 +11,10 @@ export async function getServerSideProps({ res }) {
   const { repositories = [] } = profile;
   delete profile['repositories'];
 
-  const { text: raw = '' } = await readmeFetcher();
-  const { content } = parseArticleData({ raw });
-  const compiledSource = await compileSource({ content });
-  const minifiedSource = await minifySource({ compiledSource });
-
   return {
     props: {
       profile,
-      repositories,
-      source: minifiedSource
+      repositories
     }
   };
 }
@@ -47,26 +35,29 @@ export default function OpenSourcePage({ profile, repositories, source }) {
       />
       <Header />
 
-      <Title>Open Source</Title>
+      <Container clean>
+        <Title>Open Source</Title>
 
-      <div className="flex flex-col">
-        <GitHubProfile
-          avatar={avatar}
-          bio={bio}
-          company={company}
-          email={email}
-          username={username}
-          location={location}
-          url={url}
-          className="mb-12"
-        />
+        <div className="flex flex-col">
+          <GitHubProfile
+            avatar={avatar}
+            bio={bio}
+            company={company}
+            email={email}
+            username={username}
+            location={location}
+            url={url}
+            className="mb-12"
+          />
 
-        {/* Readme  */}
-        <Container clean className="flex h-full mt-12">
-          <MDXRemote compiledSource={source} components={components} />
-        </Container>
-      </div>
-
+          {/* Repositories  */}
+          <div className="container">
+            {repositories.map(repository => (
+              <RepositoryPreview key={repository.name} {...repository} />
+            ))}
+          </div>
+        </div>
+      </Container>
       <Footer />
     </>
   );
