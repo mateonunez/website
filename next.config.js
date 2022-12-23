de45f3e3b4d6678bf03ac82332ab5c22cfce19da
mateonunez <mateonunez95@gmail.com>
2022-12-23 17:24:03 +0100

    chore: improve Conent Security Policy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL || 'https://mateonunez.dev';
+
 const contentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self';
-  child-src mateonunez.dev;
-  style-src 'self' mateonunez.dev;
-  font-src 'self';
+  default-src 'self' ${baseURL} https://vercel.live https://*.vercel.com https://*.vercel-insights.com https://*.googletagmanager.com https://*.google-analytics.com https://*.analytics.google.com;
+  img-src 'self' data: https://i.scdn.co https://avatars.githubusercontent.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.vercel.com https://*.vercel-insights.com https://*.googletagmanager.com https://*.google-analytics.com https://*.analytics.google.com;
+  child-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://assets.vercel.com;
+  frame-src 'self' https://*.vercel.com https://*.vercel-insights.com https://vercel.live;
+  font-src 'self' https://fonts.gstatic.com https://assets.vercel.com;
 `;
 
 // Security Headers
@@ -31,7 +35,7 @@ const securityHeaders = [
   // Referrer Policy
   {
     key: 'Referrer-Policy',
-    value: 'same-origin'
+    value: 'origin-when-cross-origin'
   },
   // Frame Options
   {
