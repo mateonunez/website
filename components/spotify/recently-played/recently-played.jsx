04a0cc72864181678bbb9dde198440c04466145e
mateonunez <mateonunez95@gmail.com>
2023-02-14 21:40:12 +0100

    style(recently-page): improve design

--- components/spotify/recently-played/recently-played.jsx
+++ components/spotify/recently-played/recently-played.jsx
@@ -1,37 +1,73 @@
 import s from './recently-played.module.css';
 
-import config from 'lib/config';
-
-import { ChevronUp, Container, Fade, Title, TrackCard } from 'components';
-import { useRef } from 'react';
-import { useScroll } from 'lib/hooks';
-import { useState } from 'react';
+import { ChevronUp, Container, Title, TrackCard } from 'components';
+import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 
 export default function RecentlyPlayed({ items }) {
-  const trackContainerRef = useRef();
-  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const scroll = useScroll;
+  const nextIndex = () => {
+    if (currentIndex >= items.length - 1) return;
+    setCurrentIndex(currentIndex + 1);
+  };
+
+  const prevIndex = () => {
+    if (currentIndex <= 0) return;
+    setCurrentIndex(currentIndex - 1);
+  };
+
+  // Listen to keydown events
+  useEffect(() => {
+    window.addEventListener('keydown', event => {
+      if (event.key === 'ArrowRight') {
+        nextIndex();
+      }
+
+      if (event.key === 'ArrowLeft') {
+        prevIndex();
+      }
+    });
+
+    return () => {
+      window.removeEventListener('keydown', event => {
+        if (event.key === 'ArrowRight') {
+          nextIndex();
+        }
+
+        if (event.key === 'ArrowLeft') {
+          prevIndex();
+        }
+      });
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const recentlyTrack = useMemo(() => {
+    return items[currentIndex];
+  }, [currentIndex, items]);
 
   return (
     <>
-      <Container clean>
-        <Fade>
-          <Title>Recently Played</Title>
-        </Fade>
+      <Container>
+        <Title variant="subtitle">Recently Played</Title>
+      </Container>
 
-        {items.length > 0 && (
-          <div className={s.root}>
-            <button
-              className={s.navigator}
-              onClick={() => {
-                scroll(trackContainerRef, 'left');
-              }}
-              aria-label="Less Tracks">
-              <ChevronUp className="w-6 h-6 font-black transition duration-500 transform -rotate-90" />
-            </button>
+      {items.length > 0 && (
+        <div className={s.root}>
+          <button
+            className={s.navigator}
+            onClick={() => {
+              prevIndex();
+            }}
+            aria-label="Less Tracks"
+            style={{
+              left: '10px'
+            }}>
+            <ChevronUp className="w-6 h-6 font-black transition duration-500 transform -rotate-90" />
+          </button>
 
-            <div
+          {/* <div
               className={s['track-container']}
               ref={trackContainerRef}
               onTouchMove={event => {
@@ -39,25 +75,25 @@ export default function RecentlyPlayed({ items }) {
 
                 scroll(trackContainerRef, direction);
                 setPrevScrollPos(event.touches[0].clientX);
-              }}>
-              {items.map((item, key) => (
-                <Fade key={`${item.id}-${key}`} delay={key + config.munber / 100} clean>
-                  <TrackCard item={item} delay={key + config.munber / 100} />
-                </Fade>
-              ))}
-            </div>
+              }}> */}
+          <Container clean>
+            <TrackCard item={recentlyTrack} currentIndex={currentIndex} />
+          </Container>
+          {/* </div> */}
 
-            <button
-              className={s.navigator}
-              onClick={() => {
-                scroll(trackContainerRef, 'right');
-              }}
-              aria-label="More Tracks">
-              <ChevronUp className="w-6 h-6 transition duration-500 transform rotate-90 hover:scale-110" />
-            </button>
-          </div>
-        )}
-      </Container>
+          <button
+            className={s.navigator}
+            onClick={() => {
+              nextIndex();
+            }}
+            aria-label="More Tracks"
+            style={{
+              right: '10px'
+            }}>
+            <ChevronUp className="w-6 h-6 transition duration-500 transform rotate-90 hover:scale-110" />
+          </button>
+        </div>
+      )}
     </>
   );
 }
