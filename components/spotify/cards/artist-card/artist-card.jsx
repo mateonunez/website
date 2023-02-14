b667ba352e8f5e31fa108b2e36987e34bc3d6de4
mateonunez <mateonunez95@gmail.com>
2023-02-14 21:39:51 +0100

    style(artist-card): improve design

--- components/spotify/cards/artist-card/artist-card.jsx
+++ components/spotify/cards/artist-card/artist-card.jsx
@@ -1,6 +1,5 @@
 import s from './artist-card.module.css';
 
-import Image from 'next/image';
 import Link from 'next/link';
 
 import { Fade, Title } from 'components';
@@ -10,6 +9,9 @@ export default function ArtistCard({ item, delay = 0 }) {
   return (
     <>
       <div className={s.root}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={item.thumbnail} alt={item.title} className={s.image} />
+
         <Link
           href={item.url}
           passHref
@@ -17,16 +19,6 @@ export default function ArtistCard({ item, delay = 0 }) {
           rel="noopener noreferrer"
           title={`${item.name} [${item.genres}]`}
           aria-label={`${item.name} [${item.genres}]`}>
-          <div className="absolute inset-0 gradient blend-darken" />
-          <Image
-            className={s.image}
-            src={item.thumbnail}
-            alt={item.name}
-            height={320}
-            width={260}
-            layout="responsive"
-            quality={60}
-          />
           <Fade className={s['name-container']} delay={delay + config.munber / 100}>
             <Title element="h3" variant="naked" className={s.name}>
               {item.name}
