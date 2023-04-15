34d84b75d0f6385bffc7a993a57b2319928e7f98
mateonunez <mateonunez95@gmail.com>
2023-04-15 14:33:23 +0200

    feat(blog): add metadata

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,24 +1,20 @@
 import s from 'styles/pages/blog/index.module.css';
 
 import { Container, Title } from 'components';
-
-import ArticlePreview from 'components/articles/preview';
-
 import { getAllArticles } from 'lib/articles/parser';
-// import { NextSeo } from 'next-seo';
+import ArticlePreview from 'components/articles/preview';
+import meta from 'lib/config/metadata.js';
+
+export const metadata = {
+  title: '> blog',
+  description: `Articles written with ❤️ by ${meta.author.name} and the Community. ${meta.description}`,
+  keywords: [...meta.keywords, 'blog', 'articles']
+};
 
 export default function Blog() {
   const articles = getAllArticles();
   return (
     <>
-      {/* <NextSeo
-        title="Blog"
-        description="Articles written with ❤️ by Mateo Nunez."
-        openGraph={{
-          title: "Mateo's Blog"
-        }}
-      /> */}
-
       <Container clean>
         <Title>Blog</Title>
 
