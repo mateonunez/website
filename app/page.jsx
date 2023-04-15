c854b4c9709b98febe5203fb7986e15cb24fe104
mateonunez <mateonunez95@gmail.com>
2023-04-15 14:13:54 +0200

    feat(page): add metadata object

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,9 @@ import s from 'styles/pages/home.module.css';
 
 import { Hero, About } from 'components';
 import { getLastArticle } from 'lib/articles/parser';
+import metadata from 'lib/config/metadata.js';
+
+export { metadata };
 
 export default async function HomePage() {
   const article = getLastArticle();
