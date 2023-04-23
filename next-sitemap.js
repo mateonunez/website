3881a8de9fa70434d965d53dfbd5b2fbc2aa7b1a
Mateo Nunez <mateonunez95@gmail.com>
2023-04-23 12:25:14 +0200

    build(sitemap): generate article slugs
    Signed-off-by: Mateo Nunez <mateonunez95@gmail.com>

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_BASE_URL || 'https://mateonunez.dev',
-  generateRobotsTxt: true
+  generateRobotsTxt: true,
+  additionalPaths: async config => {
+    const result = [];
+    const articleSlugs = fs
+      .readdirSync(path.join(process.cwd(), './articles'))
+      .filter(file => /\.mdx?$/.test(file))
+      .map(file => file.replace(/\.mdx?$/, ''));
+
+    for (const slug of articleSlugs) {
+      result.push({
+        loc: `/blog/${slug}`,
+        changefreq: 'yearly',
+        priority: 0.7,
+        lastmod: new Date().toISOString()
+      });
+    }
+
+    // result.push({
+    //   loc: '/blog',
+    //   changefreq: 'yearly',
+    //   priority: 0.7,
+    //   lastmod: new Date().toISOString(),
+
+    //   // article slugs
+
+    // });
+
+    return result;
+  }
 };
