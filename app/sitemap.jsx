5b911545c6d7a30fd9b7f7e788cece04f3b3c4f3
Mateo Nunez <mateonunez95@gmail.com>
2023-04-29 11:00:19 +0200

    feat(sitemap): generate with Next.js (#246)
    Signed-off-by: Mateo Nunez <mateonunez95@gmail.com>

---
+++ app/sitemap.jsx
@@ -0,0 +1,44 @@
+import fs from 'fs';
+import config from 'lib/config';
+import path from 'path';
+
+function generateArticlesSitemap() {
+  const result = [];
+  const articleSlugs = fs
+    .readdirSync(path.join(process.cwd(), './articles'))
+    .filter(file => /\.mdx?$/.test(file))
+    .map(file => file.replace(/\.mdx?$/, ''));
+
+  for (const slug of articleSlugs) {
+    result.push({
+      url: new URL(`/blog/${slug}`, config.baseUrl).toString(),
+      lastModified: new Date()
+    });
+  }
+
+  return result;
+}
+
+export default function sitemap() {
+  const articles = generateArticlesSitemap();
+
+  return [
+    {
+      url: new URL('/', config.baseUrl).toString(),
+      lastModified: new Date()
+    },
+    {
+      url: new URL('/blog', config.baseUrl).toString(),
+      lastModified: new Date()
+    },
+    ...articles,
+    {
+      url: new URL('/open-source', config.baseUrl).toString(),
+      lastModified: new Date()
+    },
+    {
+      url: new URL('/spotify', config.baseUrl).toString(),
+      lastModified: new Date()
+    }
+  ];
+}
