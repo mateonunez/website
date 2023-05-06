8724199d00589876c14268ad72a2ff404986e82e
Mateo Nunez <mateonunez95@gmail.com>
2023-05-06 15:22:09 +0200

    chore: remove generateMetadata method
    Signed-off-by: Mateo Nunez <mateonunez95@gmail.com>

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -3,48 +3,48 @@ import s from 'styles/pages/blog/[slug].module.css';
 import Article from 'components/articles';
 import { cache } from 'react';
 import { getArticle } from 'lib/articles/parser';
-import config from 'lib/config';
-import meta from 'lib/config/metadata.js';
+// import config from 'lib/config';
+// import meta from 'lib/config/metadata.js';
 
 const fetchArticle = cache(async ({ slug }) => {
   const { frontMatter, source } = await getArticle({ slug });
   return { frontMatter, source };
 });
 
-export async function generateMetadata({ params }) {
-  const { slug } = params;
-  const { frontMatter } = await fetchArticle({ slug });
+// export async function generateMetadata({ params }) {
+//   const { slug } = params;
+//   const { frontMatter } = await fetchArticle({ slug });
 
-  const baseUrl = new URL(config.baseUrl);
-  const imagePath = frontMatter.image.startsWith('/') ? frontMatter.image : '/' + frontMatter.image;
-  const imageUrl = new URL(imagePath, baseUrl).toString();
+//   const baseUrl = new URL(config.baseUrl);
+//   const imagePath = frontMatter.image.startsWith('/') ? frontMatter.image : '/' + frontMatter.image;
+//   const imageUrl = new URL(imagePath, baseUrl).toString();
 
-  const dynamicMetadata = {
-    ...meta,
-    title: frontMatter.title,
-    description: frontMatter.description,
-    keywords: frontMatter.tags,
-    openGraph: {
-      ...meta.openGraph,
-      title: frontMatter.title,
-      description: frontMatter.description,
-      type: 'article',
-      article: {
-        authors: [frontMatter.author.name],
-        tags: frontMatter.tags,
-        publishedTime: frontMatter.date,
-        modifiedTime: frontMatter.date
-      },
-      images: [
-        {
-          url: imageUrl,
-          alt: frontMatter.title
-        }
-      ]
-    }
-  };
-  return dynamicMetadata;
-}
+//   const dynamicMetadata = {
+//     ...meta,
+//     title: frontMatter.title,
+//     description: frontMatter.description,
+//     keywords: frontMatter.tags,
+//     openGraph: {
+//       ...meta.openGraph,
+//       title: frontMatter.title,
+//       description: frontMatter.description,
+//       type: 'article',
+//       article: {
+//         authors: [frontMatter.author.name],
+//         tags: frontMatter.tags,
+//         publishedTime: frontMatter.date,
+//         modifiedTime: frontMatter.date
+//       },
+//       images: [
+//         {
+//           url: imageUrl,
+//           alt: frontMatter.title
+//         }
+//       ]
+//     }
+//   };
+//   return dynamicMetadata;
+// }
 
 export default async function BlogArticle({ params }) {
   'use client';
