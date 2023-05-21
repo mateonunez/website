e8fa7c36b7fa6c39197134e501d0d55e9eb37fd8
Mateo Nunez <mateonunez95@gmail.com>
2023-05-21 21:23:27 +0200

    feat(api): create repository API

---
+++ app/api/open-source/repositories/[slug]/route.js
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server';
+import { getRepository } from 'lib/github';
+
+export async function GET(request, { params }) {
+  const { slug: repository } = params;
+  if (!repository) {
+    return NextResponse.json({ error: 'Repository not found' }, { status: 404 });
+  }
+
+  const { [repository]: repositoryFetched } = await getRepository(repository).catch(err => {
+    return NextResponse.json(
+      { message: 'Something went wrong with GitHub.', extra: err },
+      { status: 500 }
+    );
+  });
+
+  if (!repositoryFetched) {
+    return NextResponse.json({ error: 'Github not available' }, { status: 500 });
+  }
+
+  return NextResponse.json({ [repository]: repositoryFetched });
+}
