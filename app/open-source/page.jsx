31beb38431a3bb32dd79ccb4b6f4dfc333ca3ec8
Mateo Nunez <mateonunez95@gmail.com>
2023-05-21 21:25:33 +0200

    chore(open-source): refactor page
    WIP

--- app/open-source/page.jsx
+++ app/open-source/page.jsx
@@ -1,6 +1,12 @@
+import s from 'styles/pages/open-source/page.module.css';
+
 import { Container, Fade, Followers, GitHubProfile, RepositoryPreview, Title } from 'components';
-import { profileFetcher } from 'lib/fetchers/open-source/fetcher';
+import { WordAnimator } from 'components';
+import { profileFetcher, repositoryFetcher } from 'lib/fetchers/open-source/fetcher';
 import meta from 'lib/config/metadata.js';
+import cn from 'classnames';
+import Image from 'next/image';
+import config from 'lib/config';
 
 export const metadata = {
   title: '> open source',
@@ -15,59 +21,154 @@ export const metadata = {
 };
 
 export default async function OpenSourcePage() {
+  const words = ['', 'Art', 'People', 'Code', 'Passion', 'You'];
+
   const profile = await profileFetcher();
-  const { repositories = [], followers = [] } = profile;
+  const { sponsors = [], repositories = [], followers = [] } = profile;
+
+  followers.sort(() => Math.random() - 0.5);
+  const followersShown = followers.slice(0, config.munber);
 
   delete profile['repositories'];
   delete profile['followers'];
+  delete profile['sponsors'];
 
-  followers.sort(() => Math.random() - 0.5);
-
-  const { avatar, bio, company, email, username, location, url } = profile;
+  // const { avatar, bio, company, email, username, location, url } = profile;
 
   return (
     <>
-      {/* <NextSeo
-        title="Open Source"
-        description="Open Source projects made with ❤️ by Mateo Nunez."
-        openGraph={{
-          title: "Mateo's Open Source Projects by GitHub"
-        }}
-      /> */}
+      <Container>
+        <Title variant="title-secondary">Open Source Is...</Title>
+
+        <div className={s.welcomeContainer}>
+          <div />
+          <WordAnimator words={words} clickable />
+          <div />
+        </div>
+      </Container>
+
+      {/* Sponsors */}
+      <Container clean id="you">
+        <div className={s.thanksContainer}>
+          <Fade direction="left" delay={0.5} clean className="w-full">
+            <div class="flex flex-col items-center justify-center w-full p-4 m-4">
+              <h2 className="text-4xl font-bold text-center">Thank you.</h2>
+
+              <article className="prose lg:prose-xl" style={{ margin: '2rem' }}>
+                Special thanks to my sponsors, thanks to them who help me carry out all my projects
+                and dreams. Thank you for your support and for believing in me.
+              </article>
+
+              {/* Create a section for the sponsors */}
+              <div className="flex flex-wrap items-center justify-between w-full">
+                {sponsors.map(sponsor => (
+                  <div
+                    key={sponsor.login}
+                    className="flex flex-col items-center justify-center w-1/2 p-4 m-4"
+                    style={{ margin: 'auto' }}>
+                    <a
+                      href={sponsor.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="mt-4 text-lg font-bold text-center">
+                      {/* eslint-disable-next-line @next/next/no-img-element */}
+                      <img
+                        src={sponsor.avatar}
+                        alt={sponsor.username}
+                        className="w-24 h-24 rounded-full shadow-lg"
+                      />
+                      @{sponsor.username}
+                    </a>
+
+                    <div className="text-sm">
+                      <RepositoryPreview
+                        key={sponsor.repositories[0].name}
+                        style={{ maxWidth: '21rem' }}
+                        {...sponsor.repositories[0]}
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+              {/* Consider sposoring me section */}
+              <div className="flex flex-col items-center justify-center w-full p-4 m-4">
+                <h2 className="text-4xl font-bold text-center">Consider sponsoring me.</h2>
+
+                <article className="prose lg:prose-xl" style={{ margin: '2rem' }}>
+                  If you like my work and want to support me, consider sponsoring me on GitHub.
+                </article>
+
+                <div className="flex flex-wrap items-center justify-between w-full">
+                  <div
+                    className="flex flex-col items-center justify-center w-1/2 p-4 m-4"
+                    style={{ margin: 'auto' }}>
+                    <a
+                      href="https://github.com/sponsors/mateonunez"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="mt-4 text-lg font-bold text-center">
+                      {/* eslint-disable-next-line @next/next/no-img-element */}
+                      <img
+                        src="https://github.githubassets.com/images/modules/site/sponsors/pixel-mona-heart.gif"
+                        alt="Sponsor me"
+                        className="w-24 h-24 rounded-full shadow-lg"
+                      />
+                      Sponsor me
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Fade>
+        </div>
+      </Container>
+
+      {/* Followers */}
       <Container clean>
-        <Title>Open Source</Title>
+        <div className={s.followersContainer}>
+          <Fade direction="left" delay={0.5} className="w-2/3">
+            <h2 className="text-4xl font-bold text-center">Followers</h2>
+            <article className="prose text-center lg:prose-xl" style={{ margin: '2rem' }}>
+              Thanks to every single person that has contributed on growing this passion of mine.
+            </article>
 
-        <div className="flex flex-col">
-          <Container>
-            <Fade delay={0.3}>
-              <GitHubProfile
-                avatar={avatar}
-                bio={bio}
-                company={company}
-                email={email}
-                username={username}
-                location={location}
-                url={url}
-                className="mb-24"
-              />
-            </Fade>
-          </Container>
-
-          {/* Followers  */}
-          <Container>
-            <Fade delay={1.3}>
-              <Followers followers={followers} />
-            </Fade>
-          </Container>
-
-          {/* Repositories  */}
-          <Container>
-            <Fade delay={3}>
-              {repositories.map(repository => (
-                <RepositoryPreview key={repository.name} {...repository} />
+            {/* You can be here */}
+            <div className={s.followers}>
+              {followersShown.map(follower => (
+                <div key={follower.login} className="p-4 m-4">
+                  <a
+                    href={follower.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex flex-col items-center">
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img
+                      src={follower.avatar}
+                      alt={follower.username}
+                      className="w-24 h-24 rounded-full shadow-lg"
+                      height={128}
+                      width={128}
+                    />
+                    <span className="mt-2 text-sm text-center text-caption">
+                      @{follower.username}
+                    </span>
+                  </a>
+                </div>
               ))}
-            </Fade>
-          </Container>
+            </div>
+
+            {/* Y'all */}
+            <div className="text-center">
+              <a
+                href="https://github.com/mateonunez?tab=followers"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-lg font-bold text-center">
+                and {followers.length - followersShown.length} more...
+              </a>
+            </div>
+          </Fade>
         </div>
       </Container>
     </>
