154c233700befedec8d3ae2048caa9737670da6f
mateonunez <mateonunez95@gmail.com>
2024-01-08 22:16:30 +0100

    chore: some improvements
    Signed-off-by: mateonunez <mateonunez95@gmail.com>

--- components/layouts/main/main.jsx
+++ components/layouts/main/main.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Header, Footer } from 'components';
+import { Header, Footer, ChevronUp } from 'components';
 import { Analytics } from 'components/analytics';
 
 export default function MainLayout({ children }) {
@@ -9,6 +9,14 @@ export default function MainLayout({ children }) {
       <Header />
       <main>{children}</main>
       <Footer />
+
+      {/* TODO: refactor this */}
+      <div className="fixed bottom-0 right-0 z-50 mb-4 mr-4">
+        <a href="#top" className="text-white hover:scale-200 transform transition-all duration-300">
+          <ChevronUp className="w-8 h-8" />
+        </a>
+      </div>
+
       <Analytics />
     </>
   );
