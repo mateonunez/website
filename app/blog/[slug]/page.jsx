8745de56c8d6a62a6528072a598c2f39b4554d9f
mateonunez <mateonunez95@gmail.com>
2024-01-14 21:42:19 +0100

    wip
    Signed-off-by: mateonunez <mateonunez95@gmail.com>

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,8 +1,43 @@
-'use client';
-
 import Article from "components/articles"
 
+export async function generateMetadata({ params }) {
+  const { slug } = params;
+  const { metadata: frontMatter } = await import(`./../../../articles/${slug}.mdx`);
+
+  const baseUrl = new URL(config.baseUrl);
+  const imagePath = frontMatter.image.startsWith('/') ? frontMatter.image : `/${frontMatter.image}`;
+  const imageUrl = new URL(imagePath, baseUrl).toString();
+
+  const dynamicMetadata = {
+    ...meta,
+    title: frontMatter.title,
+    description: frontMatter.description,
+    keywords: frontMatter.tags,
+    openGraph: {
+      ...meta.openGraph,
+      title: frontMatter.title,
+      description: frontMatter.description,
+      type: 'article',
+      article: {
+        authors: [frontMatter.author.name],
+        tags: frontMatter.tags,
+        publishedTime: frontMatter.date,
+        modifiedTime: frontMatter.date,
+      },
+      images: [
+        {
+          url: imageUrl,
+          alt: frontMatter.title,
+        },
+      ],
+    },
+  };
+  return dynamicMetadata;
+}
+
 export default async function BlogArticle({ params }) {
+  'use client';
+
   const { slug } = params
   const { default: BlogArticle, metadata } = await import(`./../../../articles/${slug}.mdx`)
 
