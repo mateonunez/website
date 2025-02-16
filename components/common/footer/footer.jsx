bc37450d35fb595d77da3621306047efb518609f
mateonunez <mateonunez95@gmail.com>
2025-02-16 14:42:04 +0100

    chore: lint and fix
    Signed-off-by: mateonunez <mateonunez95@gmail.com>

--- components/common/footer/footer.jsx
+++ components/common/footer/footer.jsx
@@ -2,13 +2,13 @@ export default function Footer() {
   return (
     <footer className="p-4 mt-auto text-white sm:text-center">
       <p className="text-xs">
-        Made with 
+        Made with
         <span role="img" aria-label="heart">
           ❤️
         </span>
       </p>
       <p className="text-xs">
-        &copy; {new Date().getFullYear()} 
+        &copy; {new Date().getFullYear()}
         <a href="https://x.com/mmateonunez" alt="Mateo on Twitter" target="_blank" rel="noopener noreferrer">
           Mateo Nunez.
         </a>
