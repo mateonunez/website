75f1334a4a5a8bf274bc366df34a326d0076a857
mateonunez <mateonunez95@gmail.com>
2025-02-23 18:07:08 +0100

    perf(terminal): improvements for mobile pt2
    Signed-off-by: mateonunez <mateonunez95@gmail.com>

--- components/mate/terminal/terminal.tsx
+++ components/mate/terminal/terminal.tsx
@@ -171,8 +171,13 @@ const TerminalInput = memo(
       <input
         ref={ref}
         type="text"
-        className="flex-1 bg-transparent border-none outline-none text-white caret-amber-500 min-w-0"
+        className="flex-1 bg-transparent border-none outline-none text-white caret-amber-500 min-w-0 text-base md:text-sm"
         placeholder="Type a command..."
+        autoComplete="off"
+        autoCorrect="off"
+        autoCapitalize="none"
+        inputMode="search"
+        enterKeyHint="send"
         {...props}
       />
     </div>
