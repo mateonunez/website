eaea9575a50108add68017449a83039d0cc6d33a
mateonunez <mateonunez95@gmail.com>
2025-03-04 10:12:12 +0100

    fix: sitemap generation
    Signed-off-by: mateonunez <mateonunez95@gmail.com>

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,10 @@
 import fs from 'node:fs';
 import config from '@/lib/config';
 import path from 'node:path';
+import type { MetadataRoute } from 'next';
 
-function generateArticlesSitemap(): Sitemap[] {
-  const result: Sitemap[] = [];
+function generateArticlesSitemap(): MetadataRoute.Sitemap {
+  const result: MetadataRoute.Sitemap = [];
   const articleSlugs = fs
     .readdirSync(path.join(process.cwd(), './articles'))
     .filter((file) => /\.mdx?$/.test(file))
@@ -13,37 +14,48 @@ function generateArticlesSitemap(): Sitemap[] {
     result.push({
       url: new URL(`/blog/${slug}`, config.baseUrl).toString(),
       lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7
     });
   }
 
   return result;
 }
 
-type Sitemap = {
-  url: string;
-  lastModified: Date;
-};
-
-export default function sitemap(): Sitemap[] {
+export default function sitemap(): MetadataRoute.Sitemap {
   const articles = generateArticlesSitemap();
 
   return [
     {
       url: new URL('/', config.baseUrl).toString(),
       lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1
     },
     {
       url: new URL('/blog', config.baseUrl).toString(),
       lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8
     },
     ...articles,
     {
       url: new URL('/open-source', config.baseUrl).toString(),
       lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8
+    },
+    {
+      url: new URL('/open-source/projects', config.baseUrl).toString(),
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7
     },
     {
       url: new URL('/spotify', config.baseUrl).toString(),
       lastModified: new Date(),
-    },
+      changeFrequency: 'daily',
+      priority: 0.8
+    }
   ];
 }
