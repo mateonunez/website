fafae4b576d78626cae4d7768d70cf4315e51485
mateonunez <mateonunez95@gmail.com>
2025-09-23 16:54:31 +0200

    chore: more improvements
    Signed-off-by: mateonunez <mateonunez95@gmail.com>

--- components/mate/article-preview.tsx
+++ components/mate/article-preview.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { CalendarDays } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
--- components/providers/analytics-provider.tsx
+++ components/providers/analytics-provider.tsx
@@ -16,24 +16,13 @@ export function AnalyticsProvider({ children }: AnalyticsWrapperProps): JSX.Elem
   return (
     <>
       {gtmId && (
-        <>
-          <Script id={`gtm-init-${gtmScriptId}`} strategy="afterInteractive">
+          <Script id={`gtm-init-${gtmScriptId}`} strategy="lazyOnload">
             {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${gtmId}');`}
           </Script>
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
-              height="0"
-              width="0"
-              title="Google Tag Manager"
-              style={{ display: 'none', visibility: 'hidden' }}
-            />
-          </noscript>
-        </>
       )}
       {children}
       <Analytics />
